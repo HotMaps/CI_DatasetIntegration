c818f32b94e67569f446cec137df4b3190342f79
hud <authentication.dh@gmail.com>
2018-02-23 11:39:27 +0100

    Update secrets.py example

--- ci_secrets/secrets-example.py
+++ ci_secrets/secrets-example.py
@@ -2,4 +2,12 @@
 # This file should contain all secret variables needed by the script (passwords, etc.).
 # This file is an example of how the secrets.py file should be formatted.
 
-DB_password='password'
+# Database connection
+DB_host = 'localhost'
+DB_port = 5432
+DB_user = 'postgres'
+DB_database = 'mydb'
+DB_password = 'password'
+
+# Git repositories
+GIT_base_path = '/path/to/my/repos'
