36e8813afeaa4f42d25839720f6174d369e56a97
matthieudayer <matthieu.dayer@hevs.ch>
2018-03-06 03:13:56 -0800

    Clone all repositories

---
+++ ci_secrets/cloneAllrepositories.py
@@ -0,0 +1,36 @@
+
+
+#check repository on gitlab
+#date = datetime.datetime.utcnow()-datetime.timedelta(days=1)
+date = datetime.datetime(2010, 1, 1, 0, 0, 0)
+dateStr = date.isoformat(sep='T', timespec='seconds')+'Z'
+gl = gitlab.Gitlab('https://gitlab.com', private_token='f-JzjmRRnxzwqC5o3zsQ')
+
+group = gl.groups.get('1354895')
+
+projects = group.projects.list(all=True)
+
+for project in projects:
+
+    proj = gl.projects.get(id=project.id)
+
+    commits = proj.commits.list(since=dateStr)
+    if len(commits)==0:
+        print('No commit')
+    else:
+        repository_name = proj.name
+        repository_path = os.path.join(repositories_base_path, repository_name)
+        print(repository_name)
+        if os.path.exists(repository_path):
+            # git pull
+            print('update repository')
+            g = git.cmd.Git(repository_path)
+            g.pull()
+            print('successfuly updated repository')
+
+        else:
+            # git clone
+            print('clone repository')
+            url = proj.ssh_url_to_repo
+            Repo.clone_from(url, repository_path)
+            print('successfuly cloned repository')
