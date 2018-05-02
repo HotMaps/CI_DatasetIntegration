8306a7fc3319e5833aaa5a1d4487e61d8ceb0ec3
matthieudayer <matthieu.dayer@hevs.ch>
2018-05-02 06:30:30 -0700

    Modification of the datasets clone and update frome gitlab to have also the datasets from subgroups

--- ci_datasetintegration.py
+++ ci_datasetintegration.py
@@ -144,6 +144,7 @@ def import_shapefile(src_file, date):
             values.append(feature.GetField(att))
 
         geom = feature.GetGeometryRef()
+        https: // github.com / HotMaps / CI_DatasetIntegration.git
 
         # convert Polygon type to MultiPolygon
         if geom.GetGeometryType() == osgeo.ogr.wkbPolygon:
@@ -181,39 +182,61 @@ repo_date = datetime(2010, 1, 1, 0, 0, 0)
 dateStr = repo_date.isoformat(sep='T')+'Z'
 gl = gitlab.Gitlab('https://gitlab.com', private_token='f-JzjmRRnxzwqC5o3zsQ')
 
+hotmapsGroups = []
+listOfRepositories = []
+
+allGroups = gl.groups.list()
+
 group = gl.groups.get('1354895')
+hotmapsGroups.append(group)
+print(group.id)
 
-projects = group.projects.list(all=True)
+subgroups = group.subgroups.list()
 
-for project in projects:
 
-    proj = gl.projects.get(id=project.id)
+# Add all subgroups in the groups list as groups
+for subgroup in subgroups:
+    hotmapsGroups.append(gl.groups.get(subgroup.id, lazy=True))
 
-    commits = proj.commits.list(since=dateStr)
-    if len(commits)==0:
-        print('No commit')
-    else:
-        try:
-            f = proj.files.get(file_path='datapackage.json', ref='master')
-            repository_name = proj.name
-            listOfRepositories.append(repository_name)
-            repository_path = os.path.join(repositories_base_path, repository_name)
-            print(repository_name)
-            if os.path.exists(repository_path):
-                # git pull
-                print('update repository')
-                g = git.cmd.Git(repository_path)
-                g.pull()
-                print('successfuly updated repository')
 
-            else:
-                # git clone
-                print('clone repository')
-                url = proj.http_url_to_repo
-                Repo.clone_from(url, repository_path)
-                print('successfuly cloned repository')
-        except:
-            print('No datapackage.json or in a wrong place')
+for group in hotmapsGroups:
+        projects = group.projects.list(all=True)
+        print(projects)
+
+        for project in projects:
+
+            proj = gl.projects.get(id=project.id)
+
+            commits = proj.commits.list(since=dateStr)
+            #print(proj)
+            try:
+               #f = proj.files.get(file_path='datapackage.json', ref='master')
+               #print(f.content)
+
+               if len(commits) == 0:
+                   print('No commit')
+               else:
+                   repository_name = proj.name
+                   repository_path = os.path.join(repositories_base_path, repository_name)
+                   listOfRepositories.append(proj.name)
+                   print(repository_name)
+
+                   if os.path.exists(repository_path):
+                       # git pull
+                       print('update repository')
+                       g = git.cmd.Git(repository_path)
+                       g.pull()
+                       print('successfuly updated repository')
+
+                   else:
+                       # git clone
+                       print('clone repository')
+                       url = proj.http_url_to_repo
+                       #print(url)
+                       Repo.clone_from(url, repository_path)
+                       print('successfuly cloned repository')
+            except:
+                print("No datapackage.json or in the wrong place")
 
 
 listOfRepositories.append('.git')
@@ -458,7 +481,7 @@ for repository_name in listOfRepositories:
                        '-targetDir ' + pyramid_path + ' ' + raster_path
                 print(cmds)
                 subprocess.call(cmds, shell=True)
-
+p = group.projects.
                 # add to geoserver
 
                 workspace = 'hotmaps'
--- ci_secrets/cloneAllRepositories.py
+++ ci_secrets/cloneAllRepositories.py
@@ -1,5 +1,5 @@
 # python 3.5
-import datetime
+from datetime import datetime
 import gitlab
 import os.path
 import git
@@ -10,38 +10,68 @@ repository_name = ''
 repository_path = os.path.join(repositories_base_path, repository_name)
 
 # check repository on gitlab
-#date = datetime.datetime.utcnow()-datetime.timedelta(days=1)
-date = datetime.datetime(2010, 1, 1, 0, 0, 0)
-dateStr = date.isoformat(sep='T')+'Z'
+#repo_date = datetime.utcnow()-timedelta(days=1)
+repo_date = datetime(2010, 1, 1, 0, 0, 0)
+dateStr = repo_date.isoformat(sep='T')+'Z'
 gl = gitlab.Gitlab('https://gitlab.com', private_token='f-JzjmRRnxzwqC5o3zsQ')
 
+hotmapsGroups = []
+listOfRepositories = []
+
+allGroups = gl.groups.list()
+
 group = gl.groups.get('1354895')
+hotmapsGroups.append(group)
+#print(group.id)
 
-projects = group.projects.list(all=True)
+subgroups = group.subgroups.list()
 
-for project in projects:
 
-    proj = gl.projects.get(id=project.id)
+# Add all subgroups in the groups list as groups
+for subgroup in subgroups:
+    hotmapsGroups.append(gl.groups.get(subgroup.id, lazy=True))
 
-    commits = proj.commits.list(since=dateStr)
-    if len(commits) == 0:
-        print('No commit')
-    else:
-        repository_name = proj.name
-        repository_path = os.path.join(repositories_base_path, repository_name)
-        print(repository_name)
 
-        if os.path.exists(repository_path):
-            # git pull
-            print('update repository')
-            g = git.cmd.Git(repository_path)
-            g.pull()
-            print('successfuly updated repository')
+for group in hotmapsGroups:
+        projects = group.projects.list(all=True)
+        #print(projects)
 
-        else:
-            # git clone
-            print('clone repository')
-            url = proj.http_url_to_repo
-            print(url)
-            Repo.clone_from(url, repository_path)
-            print('successfuly cloned repository')
+        for project in projects:
+
+            proj = gl.projects.get(id=project.id)
+
+            commits = proj.commits.list(since=dateStr)
+            #print(proj)
+            try:
+               #f = proj.files.get(file_path='datapackage.json', ref='master')
+               #print(f.content)
+
+               if len(commits) == 0:
+                   print('No commit')
+               else:
+                   repository_name = proj.name
+                   repository_path = os.path.join(repositories_base_path, repository_name)
+                   listOfRepositories.append(proj.name)
+                   print(repository_name)
+
+                   if os.path.exists(repository_path):
+                       # git pull
+                       print('update repository')
+                       g = git.cmd.Git(repository_path)
+                       g.pull()
+                       print('successfuly updated repository')
+
+                   else:
+                       # git clone
+                       print('clone repository')
+                       url = proj.http_url_to_repo
+                       #print(url)
+                       Repo.clone_from(url, repository_path)
+                       print('successfuly cloned repository')
+            except:
+                print("No datapackage.json or in the wrong place")
+
+
+listOfRepositories.append('.git')
+print(listOfRepositories)
+print(len(listOfRepositories))
